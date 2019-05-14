e9078a6b7e006fc2b324463730cca466aa034452
Amaran <amardeep.chalmersuniversity@gmail.com>
2019-05-14 22:51:25 +0530

    Just another file added oi.sh

---
+++ oi.sh
@@ -0,0 +1,19 @@
+
+
+#===================================================
+#           This is just a regular title
+#===================================================
+
+#!/bin/bash
+
+clear
+
+echo "Bash Directory       : $BASH"
+echo "Bash Version         : $BASH_VERSION"
+echo "Home Directoy        : $HOME"
+echo "Current Directory    : $PWD"
+echo "PID                  : $$"
+echo ""
+
+
+ 
